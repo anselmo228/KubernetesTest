18be68c978a0d3c1cc39dec81f8c32302db9a722
Chung Hee chan <anselmo228@naver.com>
2024-04-12 13:18:30 +0900

    Update KubernetesTsetApplication.java

--- src/main/java/kubernetes_tset/kubernetes_tset/KubernetesTsetApplication.java
+++ src/main/java/kubernetes_tset/kubernetes_tset/KubernetesTsetApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KubernetesTsetApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(KubernetesTsetApplication.class, args);
 	}
